6d5c4bec17f01d60857479b741870fce3ca189bf
张世泽 <814135975@qq.com>
2017-09-11 22:13:33 +0800

    code more
    code more

---
+++ pyzioTest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#File name:
+#   pyzioTest.py
+#
+#Describe:
+#
+#   This file is used to test the lib file pyzio.py. The test
+#cases can also be founded in test docs.
+#
+#File start at:
+#   2017-09-11
+#
+#Last edit at:
+#   2017-09-11
+#
+#Author:
+#   Yeo Zach
+#
+#Contact me by:
+#   zsz100e@126.com
+#
+#ALL rights reserved
+#
+
+import pyzio
+
+#
+#function "ioD1Test":
+#
+def ioD1Test( ):
+
+    a = range( 0, 10 )
